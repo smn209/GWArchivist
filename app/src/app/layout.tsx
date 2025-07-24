2cee6c8ec49993091aebf5abe8ed84a3cc6cdcf2
kd <116354677+kd209@users.noreply.github.com>
2025-07-24 21:32:35 +0200

    feat: layout front setup

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Header } from "../components/Header"
 
 export const metadata: Metadata = {
   title: "GW",
   description: "GW",
+  icons: {
+    icon: '/skills/349.jpg',
+    apple: '/skills/349.jpg',
+  },
 };
 
 export default function RootLayout({
@@ -14,8 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Header />
         {children}
       </body>
     </html>
-  );
+  )
 }
